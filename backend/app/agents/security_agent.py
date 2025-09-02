83637491986adf31a77b7f078f29d93066c09f2c
Nenuka-22 <IT22169426@my.sliit.lk>
2025-09-02 22:31:13 +0530

    topic-keyword-add

--- backend/app/agents/security_agent.py
+++ backend/app/agents/security_agent.py
@@ -6,3 +6,28 @@ from typing import Dict
 
 from ..services import rules_guardrails
 from ..utils import basic_sanitize, is_first_aid_related
+
+_OFF_TOPIC_KEYWORDS = {
+    "crypto",
+    "bitcoin",
+    "stock",
+    "stocks",
+    "invest",
+    "investment",
+    "homework",
+    "assignment",
+    "movie",
+    "film",
+    "recipe",
+    "cook",
+    "cooking",
+    "code",
+    "coding",
+    "program",
+    "programming",
+    "travel",
+    "vacation",
+    "sports",
+    "basketball",
+    "football",
+}
